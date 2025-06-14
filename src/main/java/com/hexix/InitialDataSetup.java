47963d1a13b69d8798ee43b42a95cff0914124ae
Christopher Auth <christopher.auth@mercatis.com>
2025-06-14 19:41:07 +0200

    Add Postgres Docker Hub Feed

--- src/main/java/com/hexix/InitialDataSetup.java
+++ src/main/java/com/hexix/InitialDataSetup.java
@@ -19,7 +19,8 @@ public class InitialDataSetup {
         LOG.info("Anwendung startet, prüfe initiale Daten...");
 
         final List<Feed> myBlogFeeds = List.of(new Feed("https://forgejo.org/releases/rss.xml", "Forgejo Release\n\n", "Forgejo hat eine neue Version veröffentlicht: \n\n"),
-                new Feed("https://www.tagesschau.de/infoservices/alle-meldungen-100~rss2.xml", "", ""));
+                new Feed("https://www.tagesschau.de/infoservices/alle-meldungen-100~rss2.xml", "", ""),
+                new Feed("https://rss.p.theconnman.com/_/postgres.atom?includeRegex=%5E(latest%7C(1%5B6-9%5D%7C%5B2-9%5D%5Cd%7C%5Cd%7B3%2C%7D)(%5C.%5Cd%2B)*)%24", "Postges Docker Hub Release\n\n", "Postgres wurde in einer neuen Version veröffentlicht.\n\n") );
 
 
         for (Feed myBlogFeed : myBlogFeeds) {
