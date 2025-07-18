b1db76e49cf90f85ce66210ef3f1a0dde444a9eb
Christopher Auth <christopher.auth@mercatis.com>
2025-07-18 11:43:53 +0200

    ZDF Heute CSS Query Heise CSS Query

--- src/test/java/com/hexix/mastodon/JsoupTest.java
+++ src/test/java/com/hexix/mastodon/JsoupTest.java
@@ -1,5 +1,6 @@
 package com.hexix.mastodon;
 
+import com.hexix.JsoupParser;
 import io.quarkus.test.junit.QuarkusTest;
 import org.jboss.logging.Logger;
 import org.jsoup.Jsoup;
@@ -46,5 +47,28 @@ public class JsoupTest {
         LOG.debug(sj);
 
         Assertions.assertTrue(sj.toString().contains("Krebs"));
+    }
+
+    @Test
+    public void zdfheute(){
+        final String article = JsoupParser.getArticle("https://www.zdfheute.de/politik/ausland/dobrindt-eu-asylpolitik-migration-100.html");
+
+        LOG.info(article);
+
+        Assertions.assertTrue(article.contains("Gemeinsam mit fünf Amtskollegen aus anderen EU-Ländern will Bundesinnenminister"));
+        Assertions.assertTrue(article.contains("Die wichtigsten Routen für irreguläre Migranten führen über das östliche und zentrale"));
+
+        Assertions.assertTrue(article.contains("Der innenpolitische Sprecher der Grünen-Fraktion, Marcel Emmerich, meint: \"Die Streichung des Verbindungselements ist ein herzloser Angriff auf Schutzsuchende, Familien und Kinder, die in Länder ohne jede persönliche Bindung abgeschoben werden sollen.\""));
+    }
+
+    @Test
+    public void heise(){
+        final String article = JsoupParser.getArticle("https://www.heise.de/news/Oracle-309-Sicherheitsupdates-fuer-alle-moeglichen-Produkte-10490492.html");
+
+        LOG.info(article);
+
+        Assertions.assertTrue(article.contains("Oracle hat in der Nacht zum Mittwoch seinen quartalsweise stattfindenden \"Critical Patch Update\" genannten Patchday begangen. Dabei hat das Unternehmen 309 Sicherheitspatches für Produkte quer durch sein Portfolio veröffentlicht."));
+        Assertions.assertTrue(article.contains("Produkte Updates bereitstehen, die noch \"Premier Support\" erhalten oder in der erweiterten Support-Phase sind"));
+
     }
 }
