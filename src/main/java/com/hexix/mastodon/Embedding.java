119b3dca5ee00345cabbd32a209c2668a39d9c26
Christopher Auth <christopher.auth@mercatis.com>
2025-07-21 14:04:06 +0200

    Bugfix

--- src/main/java/com/hexix/mastodon/Embedding.java
+++ src/main/java/com/hexix/mastodon/Embedding.java
@@ -42,7 +42,7 @@ public class Embedding extends PanacheEntity {
     @Column(name = "embedding_created_at")
     LocalDateTime embeddingCreatedAt;
 
-    @Column(name= "lokal_embedding_vector_string", columnDefinition = "TEXT")
+    @Column(name= "local_embedding_vector_string", columnDefinition = "TEXT")
     String localEmbeddingVectorString;
 
     @Transient
