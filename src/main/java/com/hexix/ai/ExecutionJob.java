0a92d5e5604476e81821104c55e51c94aeaa3cad
Christopher Auth <christopher.auth@mercatis.com>
2025-08-20 09:09:25 +0200

    Bugfix

--- src/main/java/com/hexix/ai/ExecutionJob.java
+++ src/main/java/com/hexix/ai/ExecutionJob.java
@@ -1,6 +1,7 @@
 package com.hexix.ai;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 import java.time.LocalDateTime;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 @Entity
 public class ExecutionJob extends PanacheEntity {
 
+    @Column(columnDefinition = "TEXT")
     public String schedulerName;
     public LocalDateTime executionTime;
     public boolean completed;
