389684888688c4d2cceb4959d7f282da712fe5da
Christopher Auth <auth@hexix.de>
2026-03-11 10:54:48 +0100

    Fix Test fehler

--- src/test/java/com/hexix/mastodon/resource/client/FavouritesClientTest.java
+++ src/test/java/com/hexix/mastodon/resource/client/FavouritesClientTest.java
@@ -3,15 +3,26 @@ package com.hexix.mastodon.resource.client;
 import com.hexix.mastodon.api.MastodonDtos;
 import com.hexix.mastodon.resource.FavouritesService;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @QuarkusTest
+@TestProfile(FavouritesClientTest.NoSchedulerProfile.class)
 class FavouritesClientTest {
+
+    public static class NoSchedulerProfile implements io.quarkus.test.junit.QuarkusTestProfile {
+        @Override
+        public Map<String, String> getConfigOverrides() {
+            return Collections.singletonMap("quarkus.scheduler.enabled", "false");
+        }
+    }
 
     @Inject
     @RestClient
